21f26001d30a9da35ec31966a1b6bed7d1ce057e
Jonathan Joseph <josephj7@uw.edu>
2017-03-07 11:14:46 -0800

    TST: change metric tests. CCCSMF only converges as n_classes goes to infinity. Reduce magic numbers by assigning to helpfully named variables.

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -1,34 +1,33 @@
 import itertools
 
 import numpy as np
-import pandas as pd
 import pytest
 
 from src.metrics import *
 
 
-def rand_uniform(rs, classes=10):
-    return rs.choice(range(classes), 10000)
+def rand_uniform(rs, n_classes, n_samples):
+    return rs.choice(n_classes, n_samples)
 
 
-def rand_multinomial_flat(rs, classes=10):
-    counts = rs.multinomial(10000, np.full(classes, 0.1))
-    arr = np.repeat(range(classes), counts)
+def rand_multinomial_flat(rs, n_classes, n_samples):
+    counts = rs.multinomial(n_samples, np.full(n_classes, 0.1))
+    arr = np.repeat(range(n_classes), counts)
     rs.shuffle(arr)
     return arr
 
 
-def rand_multinomial_rand(rs, classes=10):
-    pval = rs.rand(classes)
-    counts = rs.multinomial(10000, pval / pval.sum())
-    arr = np.repeat(range(classes), counts)
+def rand_multinomial_rand(rs, n_classes, n_samples):
+    pval = rs.rand(n_classes)
+    counts = rs.multinomial(n_samples, pval / pval.sum())
+    arr = np.repeat(range(n_classes), counts)
     rs.shuffle(arr)
     return arr
 
 
-def rand_dirichelet_flat(rs, classes=10):
-    counts = rs.multinomial(10000, rs.dirichlet(np.ones(classes)))
-    arr = np.repeat(range(classes), counts)
+def rand_dirichelet_flat(rs, n_classes, n_samples):
+    counts = rs.multinomial(n_samples, rs.dirichlet(np.ones(n_classes)))
+    arr = np.repeat(range(n_classes), counts)
     rs.shuffle(arr)
     return arr
 
@@ -45,32 +44,38 @@ rand_gen_pairs = list(itertools.product(rand_generators, repeat=2))
 @pytest.mark.parametrize('rand1,rand2', rand_gen_pairs)
 def test_calc_ccc_random(rand1, rand2):
     rs = np.random.RandomState(2301598121)
-    stats = []
-    for _ in range(5000):
-        actual = rand1(rs)
-        predicted = rand2(rs, len(np.unique(actual)))
+    n_classes = 10
+    n_samples = 10000
+    n_bootstraps = 5000
+    cccs = []
+    for _ in range(n_bootstraps):
+        actual = rand1(rs, n_classes, n_samples)
+        predicted = rand2(rs, len(np.unique(actual)), n_samples)
         ccc = [calc_ccc(c, actual, predicted) for c in np.unique(actual)]
 
         # Handle unsampled classes
-        if len(ccc) < 10:
-            for i in set(range(10)).difference(actual):
+        if len(ccc) < n_classes:
+            for i in set(range(n_classes)).difference(actual):
                 ccc.insert(i, 0)
-        stats.append(ccc)
-    stats = np.array(stats).mean(0)
-    assert np.allclose(stats, 0, atol=0.005)
+        cccs.append(ccc)
+    cccs = np.array(cccs).mean(0)
+    assert np.allclose(cccs, 0, atol=0.005)
 
 
 def test_calc_cccsmf_random():
     rs = np.random.RandomState(2301598121)
-    stats = []
-    for _ in range(5000):
-        actual = rs.dirichlet(np.ones(10))
-        n = 10000
-        predicted = pd.Series(rs.choice(range(10), n)).value_counts().values/n
+    n_classes = 100
+    n_samples = 10000
+    n_bootstraps = 5000
+    cccsmfs = []
+    for _ in range(n_bootstraps):
+        actual = rs.dirichlet(np.ones(n_classes))
+        predicted = rs.choice(n_classes, n_samples)
+        predicted = np.unique(predicted, return_counts=True)[-1] / n_samples
+
         assert np.allclose(actual.sum(), predicted.sum(), 1)
         cccsmf = calc_cccsmf_accuracy_from_csmf(actual, predicted)
-        stats.append(cccsmf)
-    stats = np.mean(stats)
-    assert np.allclose(stats, 0, atol=0.005)
+        cccsmfs.append(cccsmf)
+    cccsmfs = np.mean(cccsmfs)
 
-
+    assert np.allclose(cccsmfs, 0, atol=0.005)
