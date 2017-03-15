c9f7c05e6ea26997622d30a90992b8d8d69e4aff
Jonathan Joseph <josephj7@uw.edu>
2017-03-14 17:49:49 -0700

    Move is_uncertain to utils and add calc_ui and make_mask

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 from functools import reduce
+import numbers
 import operator
 
+import numpy as np
 import pandas as pd
 from sklearn.utils.validation import check_consistent_length, column_or_1d
 
@@ -47,3 +49,77 @@ def safe_align_sequences(*seqs):
 
     check_consistent_length(*seqs)
     return [column_or_1d(s) for s in seqs]
+
+
+def is_uncertain(arr, ui=(2.5, 97.5)):
+    """Returns True if the uncertainty interval of the array contains zero
+
+    Args:
+        arr (sequence): sequence of values
+        ui (tuple of 2 floats): lower and upper bounds of uncertainty interval
+            between 0 and 100
+
+    Returns:
+        (bool)
+    """
+    lower, upper = np.percentile(arr, ui)
+    return lower < 0 and upper > 0
+
+
+def calc_ui(arr, ui=(2.5, 97.5), labels=None):
+    """Calculate the uncertainty interval around a sequence of values.
+
+    Args:
+        arr (sequence): sequence of values
+        ui (tuple of 2 floats): lower and upper bounds of uncertainty interval
+            between 0 and 100
+        labels=(tuple of 2 str): return a pandas.Series with index values set
+            to the labels
+
+    Returns:
+        ui (np.array or pd.Series)
+
+    """
+    ui = np.percentile(arr, ui)
+    if labels:
+        ui = pd.Series(ui, index=labels)
+    return ui
+
+
+def make_mask(matrix, rows, cols, ignore_errors=True):
+    """Transform dict of list matrix into a boolean numpy array.
+
+    The dict keys are assumed to be column labels and the list values
+    are assumed to be row labels.
+
+    Args:
+        matrix (dict of lists): sparse-ish matrix
+        rows (sequence): row labels
+        cols (sequence): column labels
+        errors (str): Ignore index errors if there are values in the matrix
+            which are not in the rows or columns.
+
+    Returns:
+        mask (np.array): 2D boolean array where values in matrix are True
+
+    """
+    n_rows = len(rows)
+    n_cols = len(cols)
+    if len(set(rows)) != n_rows:
+        raise ValueError('Duplicate row labels.')
+    if len(set(cols)) != n_cols:
+        raise ValueError('Duplicate column labels')
+
+    arr = np.full((n_rows, n_cols), False, dtype=bool)
+    for col, row_vals in matrix.items():
+        if ignore_errors and col not in cols:
+            continue
+        col_idx = np.where(cols == col)[0][0]
+        for row in row_vals:
+            if ignore_errors and row not in rows:
+                continue
+            row_idx = np.where(rows == row)[0][0]
+            arr[row_idx, col_idx] = True
+
+    return arr
+
