5fe25e0ffcc0461e16aedd02962b44e305b3ab22
Jonathan Joseph <josephj7@uw.edu>
2017-11-30 18:08:37 -0800

    CLN: Remove obsolete tests

--- test/conftest.py
+++ test/conftest.py
@@ -22,35 +22,3 @@ def smartva_repo(request):
 @pytest.fixture(params=['adult', 'child', 'neonate'], scope='module')
 def module(request):
     return request.param
-
-
-@pytest.fixture
-def yml_adult_cause_num_to_letter(tmpdir):
-    data = """
-adult:
-  cause:
-    1: 'A'
-    2: 'B'
-    3: 'C'
-    4: 'D'
-    5: 'E'
-"""
-    file_ = tmpdir.join('adult_cause_num_to_letter.yml')
-    file_.write(data)
-    return file_.strpath
-
-
-@pytest.fixture
-def yml_child_cause_num_to_letter(tmpdir):
-    data = """
-child:
-  cause:
-    1: 'A'
-    2: 'B'
-    3: 'C'
-    4: 'D'
-    5: 'E'
-"""
-    file_ = tmpdir.join('child_cause_num_to_letter.yml')
-    file_.write(data)
-    return file_.strpath
